65f4eb7435e8e137add26ef3ad3d34699506f3e3
tarun <tarunsuper@gmail.com>
2025-04-09 16:46:08 -0700

    scripts to convert tracking annotation xmls, and tracking prediction csvs, to format required by TrackEval

--- computer_vision_methods/tracker_eval/tracking_predictions_csv_to_txt.py
+++ computer_vision_methods/tracker_eval/tracking_predictions_csv_to_txt.py
@@ -1,12 +1,13 @@
 '''
 
-This is to create ground truth text files for our tracking annotations so that we can use the TrackEval library to obtain MOT metrics.
-
-We have point annotations from CVAT in CVAT_video export type (XML) and converts the xml data to the MOT Challenge ground truth format 
-frame_number, id, x1, y1, w, h, conf, x,y,z 
-where conf, x,y,z are all -1.
+script to read and convert output csvs from sort tracking to the .txt file format required by TrackEval
+frame_number, ant_id, x,y,w,h,conf,x,y,z
+where conf,x,y,z are -1,-1,-1,-1.
 
 
+for the ground truth xmls from CVAT, frame_number always starts from 0 in the xml (but needs to be 1 indexed for TrackEval), but for
+the predictions, I have tracking predictions on all frames. I only want to keep the ones annotated (start_frame - start_frame + 30) and subtract (start_frame-1)
+so that they start from 1 to compare with the gt.
 
 '''
 
@@ -19,35 +20,33 @@ sys.path.append('../../visualization')
 import shutil
 import cv2
 import os
-import read_xml_files_from_cvat, converting_points_to_boxes
+import pandas as pd
 
 image_width, image_height = 1920,1080
-#image_width, image_height = 1920/3,1080/3 ## for patches
 
 
+def convert_prediction_csv_to_trackeval_txt(folder_where_annotated_video_came_from, box_size, label, start_frame):
+	df = pd.read_csv(folder_where_annotated_video_came_from + label + '_yolo_tracking_with_direction.csv')
+	## only keep the ones we have annotations for
+	df = df.loc[(df.frame_number >= start_frame) & (df.frame_number < start_frame+30)]
 
+	f = open('/home/tarun/Desktop/TrackEval/data/trackers/mot_challenge/ant_tracks-val/SORT/data/' + label + '.txt', 'w')
 
-def convert_to_csv(annotation_xml_file, folder_where_annotated_video_came_from, box_size, label):
-	gts_points_dict = read_xml_files_from_cvat.xml_to_dict_cvat_for_videos(annotation_xml_file)
-	dictionary_of_boxes = {}
-	for f in gts_points_dict.keys():
-		dictionary_of_boxes[f] = []
-		dictionary_of_boxes[f] = converting_points_to_boxes.convert_points_to_boxes(gts_points_dict[f], box_size, 1920, 1080)
 	
-	f = open('/home/tarun/Desktop/TrackEval/data/gt/mot_challenge/ant_tracks-val/' + label + '/gt/gt.txt', 'w')
-    
-	for frame in dictionary_of_boxes:
-		boxes = dictionary_of_boxes[frame]
-		for b in boxes:
-			ant_id = b[0]
-			x1 = b[1]
-			y1 = b[2]
-			w = b[3] - b[1]
-			h = b[4] - b[2]
-			## frame_number is required to be 1 indexed for TrackEval
-			f.write(str(1 + frame) + ',' + str(ant_id) + ',' + str(x1) + ',' + str(y1) + ',' + str(w) + ',' + str(h) + ',-1,-1,-1,-1' + '\n')
-			
+	for index, row in df.iterrows():
+		## convert start_frame to start_frame+30 to 1-30
+		frame = row['frame_number'] - (start_frame-1)
+		ant_id = row['ant_id']
+		x1 = row['x1']
+		y1 = row['y1']
+		w = row['x2'] - row['x1']
+		h = row['y2'] - row['y1']
+
+		f.write(str(frame) + ',' + str(ant_id) + ',' + str(x1) + ',' + str(y1) + ',' + str(w) + ',' + str(h) + ',-1,-1,-1,-1' + '\n')
+
 	f.close()
+
+
 
 
 
@@ -66,9 +65,9 @@ def convert_to_csv(annotation_xml_file, folder_where_annotated_video_came_from, 
 
 
 ### Val
-convert_to_csv('/home/tarun/Desktop/antcam/downloaded_annotations_from_cvat/shack/2024-08-13_11_01_01_tracking.xml', '/media/tarun/Backup5TB/all_ant_data/shack-tree-diffuser-08-01-2024_to_08-22-2024/2024-08-13_11_01_01/', 20, '2024-08-13_11_01_01')
-convert_to_csv('/home/tarun/Desktop/antcam/downloaded_annotations_from_cvat/beer/2024-10-27_23_01_01_tracking.xml', '/media/tarun/Backup5TB/all_ant_data/beer-10-22-2024_to_10-28-2024/2024-10-27_23_01_01/', 20, '2024-10-27_23_01_01')
-convert_to_csv('/home/tarun/Desktop/antcam/downloaded_annotations_from_cvat/rain/2024-10-09_23_01_00_tracking.xml', '/media/tarun/Backup5TB/all_ant_data/rain-tree-10-03-2024_to_10-25-2024/2024-10-09_23_01_00/', 20, '2024-10-09_23_01_00')
+convert_prediction_csv_to_trackeval_txt('/media/tarun/Backup5TB/all_ant_data/shack-tree-diffuser-08-01-2024_to_08-26-2024/2024-08-13_11_01_01/', 20, '2024-08-13_11_01_01', 500)
+convert_prediction_csv_to_trackeval_txt('/media/tarun/Backup5TB/all_ant_data/beer-10-22-2024_to_11-02-2024/2024-10-27_23_01_01/', 20, '2024-10-27_23_01_01', 200)
+convert_prediction_csv_to_trackeval_txt('/media/tarun/Backup5TB/all_ant_data/rain-tree-10-03-2024_to_10-19-2024/2024-10-09_23_01_00/', 20, '2024-10-09_23_01_00', 250)
 
 ### Test
 #convert_to_yolo_labels('/home/tarun/Desktop/antcam/downloaded_annotations_from_cvat/shack/2024-08-22_03_01_01_patches.xml', '/media/tarun/Backup5TB/all_ant_data/shack-tree-diffuser-08-01-2024_to_08-22-2024/2024-08-22_03_01_01/', 20, '2024-08-22_03_01_01')
