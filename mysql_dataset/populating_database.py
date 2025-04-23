ed4cf942d01a92d0c2e2d2f0759e96cb113e4357
tarun <tarunsuper@gmail.com>
2025-04-23 16:56:16 -0700

    minor changes to add herdnet counts into db

--- mysql_dataset/populating_database.py
+++ mysql_dataset/populating_database.py
@@ -88,10 +88,10 @@ def populate_videos_table(colony, parent_folder):
 #populate_videos_table('shack', '/media/tarun/Backup5TB/all_ant_data/shack-tree-diffuser-08-26-2024_to_09-18-2024/')
 
 
-populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-08-22-2024_to_09-02-2024/')
-populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-10-03-2024_to_10-19-2024/')
-populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-10-20-2024_to_10-25-2024/')
-populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-11-15-2024_to_12-06-2024/')
+#populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-08-22-2024_to_09-02-2024/')
+#populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-10-03-2024_to_10-19-2024/')
+#populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-10-20-2024_to_10-25-2024/')
+#populate_videos_table('rain', '/media/tarun/Backup5TB/all_ant_data/rain-tree-11-15-2024_to_12-06-2024/')
 
-#populate_videos_table('beer', '/media/tarun/Backup5TB/all_ant_data/beer-tree-08-01-2024_to_08-10-2024/')
+populate_videos_table('beer', '/media/tarun/Backup5TB/all_ant_data/beer-tree-08-01-2024_to_08-10-2024/')
 #populate_videos_table('beer', '/media/tarun/Backup5TB/all_ant_data/beer-10-22-2024_to_11-02-2024/')
